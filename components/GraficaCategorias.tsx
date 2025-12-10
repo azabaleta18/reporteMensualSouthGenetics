e874ec7216013fa1ae4f15135f08681403a10430
Alfonso Zabaleta <azabaleta@southgenetics.com>
2025-12-10 14:19:10 -0300

    10/12/2025

--- components/GraficaCategorias.tsx
+++ components/GraficaCategorias.tsx
@@ -143,7 +143,7 @@ export default function GraficaCategorias({ movimientos }: GraficaCategoriasProp
               cx="50%"
               cy="50%"
               labelLine={false}
-              label={({ name, porcentaje }) => `${name}: ${porcentaje}%`}
+              label={({ name, payload }: any) => `${name}: ${payload?.porcentaje || '0.0'}%`}
               outerRadius={120}
               fill="#8884d8"
               dataKey="value"
